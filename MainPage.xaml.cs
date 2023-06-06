9371a884b1ce2ef60da511006415a0a305800079
alonsogonzalezleal <alonsogonzalez423@gmail.com>
2023-06-06 00:50:19 -0600

    Uploaded to Github

---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ToDoList;
+
+public partial class MainPage : ContentPage
+{
+    private ObservableCollection<TaskItem> tasks = new ObservableCollection<TaskItem>();
+
+    public MainPage()
+    {
+        InitializeComponent();
+        taskListView.BindingContext = this;
+    }
+
+    public ObservableCollection<TaskItem> Tasks
+    {
+        get { return tasks; }
+    }
+
+    private void OnAddTaskClicked(object sender, EventArgs e)
+    {
+        string description = taskEntry.Text;
+        if (!string.IsNullOrWhiteSpace(description))
+        {
+            TaskItem newTask = new TaskItem { Description = description };
+            tasks.Add(newTask);
+            taskEntry.Text = string.Empty;
+        }
+    }
+
+    private void OnTaskCompletedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        CheckBox checkBox = (CheckBox)sender;
+        TaskItem task = (TaskItem)checkBox.BindingContext;
+        task.IsCompleted = e.Value;
+    }
+
+    private void OnDeleteTaskClicked(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        TaskItem task = (TaskItem)button.BindingContext;
+        tasks.Remove(task);
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ToDoList;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Roboto-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("Roboto-Medium.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
